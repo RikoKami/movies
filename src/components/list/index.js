2fda3e16fc8daf8aaffcd62a839f4382366c1190
Larissa <larissa@navegarte.com.br>
2019-08-16 19:19:43 -0300

    Adicionando paginacao e imagem nao encontrada

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -4,47 +4,60 @@ import './list.scss';
 import { Link } from 'react-router-dom';
 import MovieService from '../../services/MovieService';
 
+import NoImage from '../../assets/img/noimage.png';
+
 export default function List(props){
   const [movie,setMovie] = useState([]);
   const [search,setSearch] = useState([]);
   const [genre,setGenre] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(0);
+  const pageNumbers = [];
 
   useEffect(() =>{
     async function getGenrer() {
       try {
         const resp = await MovieService.getGenres();
         setGenre(resp.data.genres);
-        // console.log(resp.data.genres);
       } catch (error) {
         
       }
     }
     getGenrer();
 
-    async function getListMovie() {
+    async function getListMovie(page) {
       try {
-        const resp = await MovieService.getListMovies(); 
+        const resp = await MovieService.getListMovies(page); 
         setMovie(resp.data.results);
         setSearch(resp.data.results);
+        setTotalPage(resp.data.total_pages);
       } catch (error) {
         
       }
     }
-    getListMovie();
+    getListMovie(page);
 
-  }, []);
+  }, [page]);
 
   useEffect(()=>{
     if(props.search.length == 0) {
-      console.log(movie);
+      // console.log(movie);
       
       setSearch(movie);
       return;
     } 
-    console.log(props.search);
+    // console.log(props.search);
     setSearch(props.search);
     
   }, [props.search]);
+
+  for (let i = page; i < page + 5; i++) {
+    if (i <= totalPage) pageNumbers.push(i);
+  }
+
+  while ( pageNumbers.length < 5) {
+    pageNumbers.push(totalPage - pageNumbers.length);
+  }
 
   return(
     <section className="list">
@@ -52,7 +65,9 @@ export default function List(props){
         <Link to={`/${item.id}`} key={item.id}>
           <figure className="card">
             <picture>
-              <img src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt=""/>
+              {
+                (item.poster_path === null) ? <img src={NoImage} alt=""/> : <img src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt=""/>
+              }
             </picture>
             <figcaption>
               <div className="cardHeader">
@@ -74,13 +89,23 @@ export default function List(props){
           </figure>
         </Link>
       ))}
-
+      
       <ul className="pagination">
-        <li className="active"><a href="">1</a></li>
-        <li><a href="">2</a></li>
-        <li><a href="">3</a></li>
-        <li><a href="">4</a></li>
-        <li><a href="">5</a></li>
+        {page != 1 &&
+          <>
+          <li><a onClick={() => {setPage(1)}}>1</a></li>
+          <span>...</span>
+          </>
+        }
+        {pageNumbers.sort().map(item => (
+          <li className={item === page ? "active" : ''} key={item}><a onClick={() => {setPage(item)}}>{item}</a></li>
+          ))}
+        {page != totalPage && 
+          <>
+            <span>...</span>
+            <li><a onClick={() => {setPage(totalPage)}}>{totalPage}</a></li>
+          </>
+        }
       </ul>
     </section>
   )
--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -17,11 +17,12 @@ export default {
   /**
    * Listagem de todos os filmes
    */
-  getListMovies(){
+  getListMovies(page){
     const config = {
       params : {
         "api_key" : "e07e4635ec0f794a9109edce2b0a4adb",
-        "language": "pt-BR"
+        "language": "pt-BR",
+        "page": page
       }
     }
     return api.get(`/discover/movie`, config);
